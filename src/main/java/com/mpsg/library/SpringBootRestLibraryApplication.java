f43b1a5e8a2d0366bf1abaff5a2cc7c153e67937
Marcos Gomes <mpg.salgad@gmail.com>
2021-04-10 21:03:39 -0400

    init

---
+++ src/main/java/com/mpsg/library/SpringBootRestLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.mpsg.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mpsg/library/SpringBootRestLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpsg.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
